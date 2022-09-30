7662fd66fcbd734f5043d7cb3f341b772d49b94a
YooInKeun <keun0390@naver.com>
2022-09-30 13:46:59 +0900

    chore: add initial project

---
+++ src/test/java/com/aiden/hackingspringbootch1reactive/HackingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiden.hackingspringbootch1reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
